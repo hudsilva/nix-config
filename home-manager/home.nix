cf4dc1b9815eb448177aeb3d2aadc87459389f34
Hudson Couto <hudson.sama@gmail.com>
2023-07-13 01:35:25 -0300

    configure direnv

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -265,7 +265,6 @@ in {
     bottom.enable = true;
     autojump.enable = true;
     bat.enable = true;
-    direnv.enable = true;
     exa.enable = true;
     fzf = {
       enable = true;
@@ -277,11 +276,32 @@ in {
       vimAlias = true;
       plugins = with pkgs.vimPlugins; [ packer-nvim vim-nix ];
     };
+
+    direnv = {
+      enable = true;
+      nix-direnv = {
+        enable = true;
+      };
+      enableZshIntegration = true;
+    };
+
     zsh = {
       enable = true;
       enableCompletion = true;
       enableAutosuggestions = true;
       enableSyntaxHighlighting = true;
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.5.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          };
+        }
+      ];
       # dotDir = ".config/zsh";
       oh-my-zsh = {
         enable = true;
@@ -319,6 +339,7 @@ in {
     tmux = {
       enable = true;
       clock24 = true;
+      shell = "${pkgs.zsh}/bin/zsh";
       plugins = with pkgs.tmuxPlugins; [
         sensible
         yank
